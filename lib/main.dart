d934b56bd4f81fe53daecd711abf0ba3d19d58f6
Mohammed-Yousef <mohammedyousefamra@gmail.com>
2024-05-26 14:04:04 +0300

    remove all unimportant comment

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,6 @@ import 'package:first_assigment/profileCard.dart';
 import 'package:first_assigment/settingsItemList.dart';
 import 'package:flutter/material.dart';
 
-/**
- * change the icons
- * change all rounded to circle avater
- * the colors of all icons
- * look how chat gpt code the settings section
- * resize the text
- * github
- */
 void main() {
   runApp(const MyApp());
 }
@@ -18,7 +10,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -31,7 +22,6 @@ class MyApp extends StatelessWidget {
         body: Container(
           margin: EdgeInsets.symmetric(horizontal: 15),
           child: Column(
-            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [ProfileCard(), OptionsRow(), SettingsItemsList()],
           ),
         ),
